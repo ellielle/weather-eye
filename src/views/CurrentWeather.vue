6a6d3ede261291f2164d319a819c89799bc56613
ellielle <40385743+ellielle@users.noreply.github.com>
2020-10-28 04:18:08 -0400

    display data

--- src/views/CurrentWeather.vue
+++ src/views/CurrentWeather.vue
@@ -1,11 +1,59 @@
 <template>
-  <h2>CURRENT WEATHER</h2>
+  <div>
+    <h6>CURRENT WEATHER</h6>
+    <div class="current-temp">{{ currentWeather.temp }}{{this.tempUnit}}</div>
+    <div class="humidity">{{ currentWeather.humidity }}%</div>
+    <div class="feels-like">{{ currentWeather.feels_like }}{{this.tempUnit}}</div>
+    <div class="description">{{ currentWeather.description }}</div>
+    <div class="wind">{{ currentWeather.wind }}{{this.windUnit}}</div>
+    <div class="icon">
+      <img :src="currentWeather.icon" alt="Weather Icon" />
+    </div>
+  </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "CurrentWeather",
-}
+
+  data() {
+    return {
+      tempUnit: "",
+      windUnit: "",
+      units: {
+        metric: {
+          temp: " °C",
+          wind: " m/s",
+        },
+        imperial: {
+          temp: " °F",
+          wind: " mph",
+        }
+      }
+    };
+  },
+
+  props: {
+    currentWeather: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    ...mapGetters(["getUserOptions"]),
+  },
+
+  created() {
+    if (this.getUserOptions.units === "imperial") {
+      this.tempUnit = this.units.imperial.temp;
+      this.windUnit = this.units.imperial.wind;
+    } else if (this.getUserOptions.units === "metric") {
+      this.tempUnit = this.units.metric.temp;
+      this.windUnit = this.units.metric.wind;
+    }
+  },
+};
 </script>
 <style lang="scss" scoped>
-
 </style>
