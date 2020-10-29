bede5ad19d2ff26fc9370c3b7b4ef547be6ce6fc
ellielle <40385743+ellielle@users.noreply.github.com>
2020-10-29 03:42:33 -0400

    add preliminary weather ui

--- src/views/WeeklyForecast.vue
+++ src/views/WeeklyForecast.vue
@@ -1,10 +1,34 @@
 <template>
-  <h2>WEEKLY FORECAST</h2>
+  <div class="container-weekly-forecast">
+    <h6>WEEKLY FORECAST</h6>
+    <div>
+      <ul v-for="day in weeklyForecast" :key="day.dt">
+        <li>{{day.temp_high}}</li>
+        <li>{{day.temp_low}}</li>
+        <li>{{day.humidity}}</li>
+        <li>{{day.description}}</li>
+        <li>{{day.wind}}</li>
+        <img :src="day.icon" alt="Weather Icon" crossorigin="anonymous">
+      </ul>
+    </div>
+  </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "WeeklyForecast",
-}
+
+  props: {
+    weeklyForecast: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  computed: {
+    ...mapGetters(["getWeatherUnits"]),
+  },
+};
 </script>
 <style lang="scss" scoped>
 
