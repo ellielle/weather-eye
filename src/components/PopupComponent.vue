7a8e5d0e8bb8707e8d2fad0e982964f655f9afcc
ellielle <40385743+ellielle@users.noreply.github.com>
2020-11-09 05:09:05 -0500

    add popup component for popup messages

---
+++ src/components/PopupComponent.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container-popup">
+  <slot></slot>
+</div>
+  
+</template>
+
+<script>
+export default {
+  name: "PopupComponent"
+}
+</script>
+
+<style lang="scss" scoped>
+.container-popup {
+  background-color: var(--sub-background);
+  border: var(--main-border);
+  padding: 15px;
+  width: fit-content;
+}
+</style>
